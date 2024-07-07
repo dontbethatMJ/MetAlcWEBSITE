dccc3984bc32af25052fd1b81f05290d640c9d2f
Tarun Gupta <tarunthegreat2003@gmail.com>
2024-07-08 00:21:11 +0530

    idk

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,139 +1,158 @@
 import { motion } from "framer-motion";
-import { BsArrowRight } from "react-icons/bs";
-import { fadeIn } from "../../variants";
 import { useState } from "react";
+import CountUp from "react-countup";
+import TeamMember from "../TeamMember";
+import Link from 'next/link';
+import Circles from "../../components/Circles";
+import { fadeIn } from "../../variants";
 
-const Contact = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
+//  data
+export const aboutData = [
+  {
+    title: "awards",
+    info: [
+      {
+        title: "Webby Awards - Honoree",
+        stage: "2011 - 2012",
+      },
+      {
+        title: "Adobe Design Achievement Awards - Finalist",
+        stage: "2009 - 2010",
+      },
+    ],
+  },
+];
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setIsLoading(true);
-
-    fetch("https://formsubmit.co/contactus@metalcproductions.com", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => {
-        alert("Thank you. I will get back to you ASAP.");
-        // Clear form data after successful submission
-        setFormData({
-          name: "",
-          email: "",
-          subject: "",
-          message: "",
-        });
-      })
-      .catch((error) => console.log(error))
-      .finally(() => setIsLoading(false));
-  };
+const About = () => {
+  const [index, setIndex] = useState(0);
 
   return (
-    <div className="h-full bg-primary/30">
-      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
-        {/* text & form */}
-        <div className="flex flex-col w-full max-w-[700px]">
-          {/* text */}
+    <div className="min-h-screen bg-primary/30 py-32 text-center xl:text-left flex flex-col overflow-hidden relative">
+      <Circles />
+
+      <div className="flex-1 container mx-auto flex flex-col items-center xl:flex-row gap-x-6 overflow-y-hidden">
+        {/* text */}
+        <div className="flex-1 flex flex-col justify-center">
           <motion.h2
-            variants={fadeIn("up", 0.2)}
+            variants={fadeIn("right", 0.2)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="h2 text-center mb-12"
+            className="h2"
           >
-            Let's <span className="text-accent">connect.</span>
+            Captivating <span className="text-accent">stories</span> birth
+            magnificent designs.
           </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+          >
+            10 years ago, I begin freelancing as a developer. Since then, I've
+            done remote work for agencies, consulted for startups, and
+            collaborated on digital products for business and consumer use.
+          </motion.p>
 
-          {/* form */}
-          <motion.form
-            variants={fadeIn("up", 0.4)}
+          {/* counters */}
+          <motion.div
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            className="flex flex-col items-center md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+          >
+            <div className="flex flex-1 md:gap-x-6">
+              {/* experience */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={10} duration={5} />
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Years of experience.
+                </div>
+              </div>
+
+              {/* clients */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={250} duration={5} />
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Satisfied clients.
+                </div>
+              </div>
+
+              {/* projects */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={650} duration={5} />
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Finished projects.
+                </div>
+              </div>
+
+              {/* awards */}
+              <div className="relative flex-1">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={8} duration={5} />
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Winning awards.
+                </div>
+              </div>
+            </div>
+          </motion.div>
+
+          {/* meet the team section */}
+          <h2 className="text-2xl md:text-4xl xl:text-5xl font-bold mb-4 md:mb-8 text-center mt-[100px]">
+            Meet the <span className="text-accent">Team</span>
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mx-auto md:ml-[350px]">
+            <TeamMember
+              key={0}
+              name="Mudit Jain"
+              description="Founder and CEO of company"
+              imageSrc="https://tse1.mm.bing.net/th?id=OIP.cxcqa9RvhGnRd8x3N2oBdgHaHn&pid=Api&P=0&h=180"
+            />
+            <TeamMember
+              key={0}
+              name="Ayush Pandey"
+              description="Founder and CEO of company"
+              imageSrc="https://tse1.mm.bing.net/th?id=OIP.cxcqa9RvhGnRd8x3N2oBdgHaHn&pid=Api&P=0&h=180"
+            />
+          </div>
+          {/* <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items -center xl:items-start">
+            
+          </div> */}
+
+          <motion.div
+            variants={fadeIn("left", 0.4)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="flex-1 flex flex-col gap-6 w-full mx-auto"
-            onSubmit={handleSubmit}
-            autoComplete="off"
-            autoCapitalize="off"
-          >
-            {/* input group */}
-            <div className="flex gap-x-6 w-full">
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                className="input"
-                value={formData.name}
-                onChange={handleChange}
-                disabled={isLoading}
-                required
-              />
-              <input
-                type="email"
-                name="email"
-                placeholder="E-mail"
-                className="input"
-                value={formData.email}
-                onChange={handleChange}
-                disabled={isLoading}
-                required
-              />
-            </div>
-            <input
-              type="text"
-              name="subject"
-              placeholder="Subject"
-              className="input"
-              value={formData.subject}
-              onChange={handleChange}
-              disabled={isLoading}
-              required
-            />
-            <textarea
-              name="message"
-              placeholder="Message..."
-              className="textarea"
-              value={formData.message}
-              onChange={handleChange}
-              disabled={isLoading}
-              required
-            />
-            <button
-              type="submit"
-              className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
-              disabled={isLoading}
-              aria-disabled={isLoading}
-            >
-              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
-                {isLoading ? "Submitting..." : "Let's talk"}
-              </span>
-
-              {!isLoading && (
-                <BsArrowRight
-                  className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"
-                  aria-hidden
-                />
-              )}
-            </button>
-          </motion.form>
+            className="hidden md:flex md:flex-col w-full md:xl:max-w-[48%] h-[480px] mx-auto  xl:ml-0"
+          ></motion.div>
         </div>
       </div>
+
+      {/* Down Arrow */}
+      <Link href="/services" passHref legacyBehavior>
+        <motion.a
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+        >
+          <img
+            src="/arrow.png"
+            alt="Down Arrow"
+            className="rotate-90 w-8 h-8 cursor-pointer hover:scale-110 transition-transform"
+          />
+        </motion.a>
+      </Link>
     </div>
   );
 };
 
-export default Contact;
+export default About;
