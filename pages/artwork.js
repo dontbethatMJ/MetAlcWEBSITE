14118c1969453bb8b9a8a6c65cd0e5f252d9f7b6
Tarun Gupta <tarun234.tg@gmail.com>
2024-09-14 01:45:24 +0530

    Create artwork.js

---
+++ pages/artwork.js
@@ -0,0 +1,16 @@
+const Artwork = () => {
+  return (
+    <div style={{ height: '100vh', width: '100vw', margin: 0, padding: 0, overflow: 'hidden' }}>
+      <iframe
+        src="https://metaversealchemists.gumroad.com/"
+        title="MetAlc Community"
+        width="100%"
+        height="100%"
+        allowFullScreen
+        style={{ border: 'none' }}
+      ></iframe>
+    </div>
+  );
+};
+
+export default Artwork;
