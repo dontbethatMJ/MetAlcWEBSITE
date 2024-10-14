a12b376f42b2c4aeab95d7bbafd5e8454de245c2
Tarun Gupta <tarun234.tg@gmail.com>
2024-10-14 22:03:48 +0530

    fixed artwork page again

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     <header className="absolute z-30 w-full items-center px-16 xl-px-0 xl:h-[90px]">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-          <Socials isArtworkPage={isArtworkPage} />
+          <Socials isArtworkPage={isArtworkPage} /> {/* Pass isArtworkPage */}
         </div>
       </div>
     </header>
