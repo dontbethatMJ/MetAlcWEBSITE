d399541a47d219468e0183fd434216c2cb2d5731
Mudit jain <91784034+MJcheonma@users.noreply.github.com>
2024-10-19 02:20:31 +0530

    Update artwork.js

--- pages/artwork.js
+++ pages/artwork.js
@@ -2,7 +2,7 @@ const Artwork = () => {
   return (
     <div style={{ height: '100vh', width: '100vw', margin: 0, padding: 0, overflow: 'hidden' }}>
       <iframe
-        src="https://community.metalcproductions.com/artwork"
+        src="https://artwork.metalcproductions.com/"
         title="MetAlc Community"
         width="100%"
         height="100%"
