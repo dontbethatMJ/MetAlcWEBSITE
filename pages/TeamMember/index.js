b31a78ad345a5010d71574237be8cd71c1e5d40d
Tarun Gupta <tarun234.tg@gmail.com>
2024-10-18 23:48:18 +0530

    more changes

--- pages/TeamMember/index.js
+++ pages/TeamMember/index.js
@@ -10,13 +10,13 @@ const TeamMember = ({name, role, description, imageSrc, link}) => {
                     className="rounded-full mb-4 w-[8rem] aspect-square mx-auto transition-transform duration-300 hover:scale-110 cursor-pointer" // Added zoom effect and pointer cursor
                 />
             </a>
-            <div className="font-bold text-xl mb-2">
+            <div className="font-bold text-xl mb-2 cursor-pointer">
                 {name}
             </div>
-            <div className="text-sm mb-2">
+            <div className="text-sm mb-2 cursor-pointer">
                 {role}
             </div>
-            <div className="text-sm mb-4">
+            <div className="text-sm mb-4 cursor-pointer">
                 {description}
             </div>
         </div>
