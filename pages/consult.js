05684d956164ebd61b6a8190b8d2bd1d2d22615f
MaybeTarun <tarunthegreat2003@gmail.com>
2025-01-25 23:33:23 +0530

    added consultation

---
+++ pages/consult.js
@@ -0,0 +1,10 @@
+const Consult = () => {
+    return (
+      <div className="bg-[url('/bg23a.webp')] bg-cover h-screen w-screen m-0 p-0">
+        oof
+      </div>
+    );
+  };
+  
+  export default Consult;
+  
